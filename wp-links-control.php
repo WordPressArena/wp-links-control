4cd24785763f9adaafa59847a5e1bd66bd6088d5
thonysmith <thony@wparena.com>
2015-12-15 14:43:58 +0700

    Create wp-links-control.php

---
+++ wp-links-control.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Links Control
+Plugin URI: https://wparena.com/
+Description: A WordPress plugin to control Internal External links in add/edit link (post editor) or in massive way.
+Version: 1.0
+Author: WordPressArena
+Author URI: https://wparena.com
+*/
